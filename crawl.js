b503af4bf779ae9f9acf3f1f638f7cbcc05e4dc2
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-03-20 11:11:07 -0300

    mais testes foram adicionados

--- crawl.js
+++ crawl.js
@@ -1,6 +1,8 @@
 function normalizeUrl(urlstring){
     const objectUrl = new URL(urlstring)
-    return (`${objectUrl.hostname}${objectUrl.pathname}`)
+    const urlnormalized = (`${objectUrl.hostname}${objectUrl.pathname}`)
+    if((urlnormalized.length > 0) && urlnormalized.slice(-1) === '/') return urlnormalized.slice(0, -1)
+    return urlnormalized
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,30 @@
 const { normalizeUrl } = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
-test('normalizeUrl test', () =>{
-    const input = 'https://mylenna/muitolinda'
+test('normalizeUrl simple test', () =>{
+    const input = 'https://vp/muitoburro'
     const actual = normalizeUrl(input)
-    const expected = 'mylenna/muitolinda'
+    const expected = 'vp/muitoburro'
     expect(actual).toEqual(expected)
 })
 
+test('normalizeUrl PathSlash test', () =>{
+    const input = 'https://vp/muitoburro/'
+    const actual = normalizeUrl(input)
+    const expected = 'vp/muitoburro'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl UpperCase test', () =>{
+    const input = 'https://VP/muitoburro'
+    const actual = normalizeUrl(input)
+    const expected = 'vp/muitoburro'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl http test', () =>{
+    const input = 'http://VP/muitoburro'
+    const actual = normalizeUrl(input)
+    const expected = 'vp/muitoburro'
+    expect(actual).toEqual(expected)
+})
